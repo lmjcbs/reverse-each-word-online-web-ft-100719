5608c8b603e77fa62404af4d842dc705339e31ef
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-10 21:31:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
-def  reverse_each_word(array)
+def  reverse_each_word(string)
+  string_array = string.split(' ')
   reversed_array = Array.new
   array.collect { |word| reversed_array << word.reverse }
   reversed_array.join(' ')
